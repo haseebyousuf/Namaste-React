9101d1603cb59e0a7119a940e39ff3956dfc408c
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-05-29 11:27:53 +0530

    lesson 1

---
+++ 01. Inception/Assignment.md
@@ -0,0 +1,4 @@
+# _Chapter 01 - Inception_
+
+## Q: What is `Emmet`?
+A: Emmet provides a set of powerful tools and shortcuts for quickly generating HTML, CSS, and other markup languages. It allows developers to write code using simple abbreviations and then expand them into complete HTML or CSS structures.
---
+++ 01. Inception/Code/App.js
@@ -0,0 +1,12 @@
+const parent = React.createElement("div", {id: "parent"},[
+  React.createElement("div", {id: "child"},[
+    React.createElement('h1',{}, "I am h1 tag"),
+    React.createElement('h2',{}, "I am h2 tag"),
+  ]),
+  React.createElement("div", {id: "child"},[
+    React.createElement('h1',{}, "I am h1 tag"),
+    React.createElement('h2',{}, "I am h2 tag"),
+  ])
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);

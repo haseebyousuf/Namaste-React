eab9889ffc99f7af666adf58356edd6e2a9cd0d4
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-20 19:37:38 +0530

    migrated to tailwind css

--- 10. Jo Dikhta Hai, Vo Bikta Hai/Code/src/components/Error.jsx
+++ 10. Jo Dikhta Hai, Vo Bikta Hai/Code/src/components/Error.jsx
@@ -3,10 +3,10 @@ const Error = () => {
   const err = useRouteError();
   console.log(err);
   return (
-    <div>
-      <h1> Oops!!!</h1>
-      <h2> Something Went Wrong</h2>
-      <p>
+    <div className='p-2 flex flex-col items-center justify-center'>
+      <h1 className='text-6xl'> Oops!!!</h1>
+      <h2 className='text-2xl p-2'> Something Went Wrong</h2>
+      <p className='text-2xl p-2'>
         {err.status} : {err.statusText}
       </p>
     </div>

4c7242ac197947a0ccd0db2d728e68603a6f49d4
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-20 19:37:52 +0530

    migrated to tailwind css

--- 10. Jo Dikhta Hai, Vo Bikta Hai/Code/src/components/RestaurantCard.jsx
+++ 10. Jo Dikhta Hai, Vo Bikta Hai/Code/src/components/RestaurantCard.jsx
@@ -12,21 +12,24 @@ const RestaurantCard = ({ resData }) => {
     deliveryTime,
   } = resData?.data;
   return (
-    <div className='res-card'>
+    <div className='rounded p-4 border-black-100  my-8 cursor-pointer flex flex-col justify-between  bg-gray-100 hover:bg-gray-200'>
       <img
+        className='rounded'
         src={CDN_URL + cloudinaryImageId}
         alt='banner-image'
         width='250px'
         height='160px'
       />
-      <h3>{name}</h3>
-      <p className='cuisines'>{cuisines.join(", ")}</p>
-      <div className='card-info'>
-        <p className='rating'>
+      <h3 className='mt-2 w-60 truncate'>{name}</h3>
+      <div className='w-60 mt-4 text-xs truncate'>{cuisines.join(", ")}</div>
+      <div className='mt-4 flex text-xs justify-between items-end'>
+        <p className='flex items-center gap-1'>
           <AiOutlineStar size='1.2rem' />
           {avgRating}
         </p>
-        <li className='delivery-time'>{deliveryTime} Mins</li>
+        <li className='flex list-disc items-center gap-1'>
+          {deliveryTime} Mins
+        </li>
         <li className='cost-for-two'> ₹{costForTwo / 100} FOR TWO</li>
       </div>
     </div>

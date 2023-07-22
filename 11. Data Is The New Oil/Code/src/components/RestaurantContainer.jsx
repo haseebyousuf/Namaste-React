352ceeb44e855c23448791274d37ea0fcb218cf4
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-22 10:36:20 +0530

    add higher order component

--- 11. Data Is The New Oil/Code/src/components/RestaurantContainer.jsx
+++ 11. Data Is The New Oil/Code/src/components/RestaurantContainer.jsx
@@ -2,14 +2,20 @@ import React, { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 import ShimmerCards from "./ShimmerCards";
 import { Link } from "react-router-dom";
+import WithPromotedLabel from "./WithPromotedLabel";
 
 const RestaurantContainer = ({ filteredResList }) => {
+  const PromotedRestaurant = WithPromotedLabel(RestaurantCard);
   return (
     <div className='flex justify-between my-8 flex-wrap'>
       {filteredResList ? (
         filteredResList.map((item) => (
           <Link key={item.data.id} to={"restaurant/" + item.data.id}>
-            <RestaurantCard resData={item} />
+            {item.data.promoted ? (
+              <PromotedRestaurant resData={item} />
+            ) : (
+              <RestaurantCard resData={item} />
+            )}
           </Link>
         ))
       ) : (

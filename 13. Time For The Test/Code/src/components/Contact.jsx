5e35ffcb0d35715f67c16067a0d70955bf76d7fe
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-03 20:28:19 +0530

    add form for testing

--- 13. Time For The Test/Code/src/components/Contact.jsx
+++ 13. Time For The Test/Code/src/components/Contact.jsx
@@ -2,9 +2,19 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className='flex flex-col items-center'>
-      <h1 className='text-3xl'> Contact Us</h1>
-      <h2 className='text-lg'> This is Contact Us page</h2>
+    <div className='flex items-center flex-col'>
+      <h1 className='text-3xl my-4'> Contact Us</h1>
+      <form className='flex items-center flex-col'>
+        <input
+          className='p-2 m-2 border border-black rounded-lg'
+          placeholder='Name'
+        />
+        <input
+          className='p-2 m-2 border border-black rounded-lg'
+          placeholder='Message'
+        />
+        <button className='p-2 m-2 bg-slate-200 rounded-lg'>Submit</button>
+      </form>
     </div>
   );
 };

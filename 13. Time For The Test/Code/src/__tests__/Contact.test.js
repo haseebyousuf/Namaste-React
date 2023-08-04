a3baed489debe0003cc2a80abe947fd9c46faaf6
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-04 11:21:33 +0530

    group related test using describe

--- 13. Time For The Test/Code/src/__tests__/Contact.test.js
+++ 13. Time For The Test/Code/src/__tests__/Contact.test.js
@@ -2,37 +2,39 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../components/Contact";
 import "@testing-library/jest-dom";
 
-test("should load heading in Contact component", () => {
-  render(<Contact />);
-  const heading = screen.getByRole("heading");
+describe("Contact Us Component Test Cases", () => {
+  test("should load heading in Contact component", () => {
+    render(<Contact />);
+    const heading = screen.getByRole("heading");
 
-  expect(heading).toBeInTheDocument();
-});
+    expect(heading).toBeInTheDocument();
+  });
 
-test("should load button in Contact component", () => {
-  render(<Contact />);
-  const button = screen.getByRole("button");
+  test("should load button in Contact component", () => {
+    render(<Contact />);
+    const button = screen.getByRole("button");
 
-  expect(button).toBeInTheDocument();
-});
+    expect(button).toBeInTheDocument();
+  });
 
-test("should load contact text in Contact component", () => {
-  render(<Contact />);
-  const text = screen.getByText("Contact Us");
+  test("should load contact text in Contact component", () => {
+    render(<Contact />);
+    const text = screen.getByText("Contact Us");
 
-  expect(text).toBeInTheDocument();
-});
+    expect(text).toBeInTheDocument();
+  });
 
-test("should load input name inside Contact component", () => {
-  render(<Contact />);
-  const nameInput = screen.getByPlaceholderText("Name");
+  test("should load input name inside Contact component", () => {
+    render(<Contact />);
+    const nameInput = screen.getByPlaceholderText("Name");
 
-  expect(nameInput).toBeInTheDocument();
-});
+    expect(nameInput).toBeInTheDocument();
+  });
 
-test("should load two input fields inside Contact component", () => {
-  render(<Contact />);
-  const inputs = screen.getAllByRole("textbox");
+  test("should load two input fields inside Contact component", () => {
+    render(<Contact />);
+    const inputs = screen.getAllByRole("textbox");
 
-  expect(inputs.length).toBe(2);
+    expect(inputs.length).toBe(2);
+  });
 });

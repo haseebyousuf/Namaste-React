c7e1fd82ba353cecd2244d9baa0bf1715791d049
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-19 10:01:37 +0530

    feat: add VideoContainer component structure

---
+++ Youtube-Clone/src/components/VideoContainer.jsx
@@ -0,0 +1,13 @@
+import FilterButtons from "./FilterButtons";
+
+const VideoContainer = () => {
+  return (
+    <div>
+      {" "}
+      <FilterButtons />
+      <h1>Video Container</h1>
+    </div>
+  );
+};
+
+export default VideoContainer;

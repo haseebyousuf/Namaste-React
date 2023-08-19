a8c9bc6783faac1e7c5b4998e77bcbd0b59fcf1b
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-19 10:00:54 +0530

    feat: add FilterButtons component structure

---
+++ Youtube-Clone/src/components/FilterButtons.jsx
@@ -0,0 +1,12 @@
+const FilterButtons = () => {
+  return (
+    <div className='flex justify-start gap-2'>
+      <button className='px-3 py-1 rounded-lg bg-slate-300'>Filter1</button>
+      <button className='px-3 py-1 rounded-lg bg-slate-300'>Filter2</button>
+      <button className='px-3 py-1 rounded-lg bg-slate-300'>Filter3</button>
+      <button className='px-3 py-1 rounded-lg bg-slate-300'>Filter4</button>
+    </div>
+  );
+};
+
+export default FilterButtons;

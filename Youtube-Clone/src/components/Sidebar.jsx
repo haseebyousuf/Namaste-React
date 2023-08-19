71bd5797a264d32c518f88844c325e7ae3d844dd
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-19 10:01:22 +0530

    feat: add Sidebar component structure

---
+++ Youtube-Clone/src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+const Sidebar = () => {
+  return (
+    <div className='w-32 mr-3 overflow-clip'>
+      <div className='p-2 border-b border-black'>Home</div>
+      <div className='p-2 border-b border-black'>Home</div>
+      <div className='p-2 border-b border-black'>Home</div>
+      <div className='p-2 border-b border-black'>Home</div>
+      <div className='p-2 border-b border-black'>Home</div>
+      <div className='p-2 border-b border-black'>Home</div>
+      <div className='p-2 border-b border-black'>Home</div>
+      <div className='p-2 border-b border-black'>Home</div>
+      <div className='p-2 border-b border-black'>Home</div>
+      <div className='p-2 border-b border-black'>Home</div>
+      <div className='p-2 border-b border-black'>Home</div>
+      <div className='p-2 border-b border-black'>Home</div>
+      <div className='p-2 border-b border-black'>Home</div>
+      <div className='p-2 border-b border-black'>Home</div>
+      <div className='p-2 border-b border-black'>Home</div>
+      <div className='p-2 border-b border-black'>Home</div>
+      <div className='p-2 border-b border-black'>Home</div>
+      <div className='p-2 border-b border-black'>Home</div>
+      <div className='p-2 border-b border-black'>Home</div>
+      <div className='p-2 border-b border-black'>Home</div>
+    </div>
+  );
+};
+
+export default Sidebar;

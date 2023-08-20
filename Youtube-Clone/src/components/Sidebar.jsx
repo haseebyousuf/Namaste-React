cfd45dc0736670d7aecc0bcf620759692f57ef6f
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-20 09:00:26 +0530

    refactor: change sidebar layout

--- Youtube-Clone/src/components/Sidebar.jsx
+++ Youtube-Clone/src/components/Sidebar.jsx
@@ -1,26 +1,49 @@
 const Sidebar = () => {
   return (
-    <div className='w-32 mr-3 overflow-clip'>
-      <div className='p-2 border-b border-black'>Home</div>
-      <div className='p-2 border-b border-black'>Home</div>
-      <div className='p-2 border-b border-black'>Home</div>
-      <div className='p-2 border-b border-black'>Home</div>
-      <div className='p-2 border-b border-black'>Home</div>
-      <div className='p-2 border-b border-black'>Home</div>
-      <div className='p-2 border-b border-black'>Home</div>
-      <div className='p-2 border-b border-black'>Home</div>
-      <div className='p-2 border-b border-black'>Home</div>
-      <div className='p-2 border-b border-black'>Home</div>
-      <div className='p-2 border-b border-black'>Home</div>
-      <div className='p-2 border-b border-black'>Home</div>
-      <div className='p-2 border-b border-black'>Home</div>
-      <div className='p-2 border-b border-black'>Home</div>
-      <div className='p-2 border-b border-black'>Home</div>
-      <div className='p-2 border-b border-black'>Home</div>
-      <div className='p-2 border-b border-black'>Home</div>
-      <div className='p-2 border-b border-black'>Home</div>
-      <div className='p-2 border-b border-black'>Home</div>
-      <div className='p-2 border-b border-black'>Home</div>
+    <div className='w-40 mr-3 overflow-clip'>
+      <div className='p-2 border-b border-black'>
+        <li className='list-none'>
+          <button className='px-3 py-2 '>Home</button>
+        </li>
+        <li className='list-none'>
+          <button className='px-3 py-2 '>Shorts</button>
+        </li>
+        <li className='list-none'>
+          <button className='px-3 py-2 '>Subscriptions</button>
+        </li>
+      </div>
+      <div className='p-2 border-b border-black'>
+        <li className='list-none'>
+          <button className='px-3 py-2 '>Library</button>
+        </li>
+        <li className='list-none'>
+          <button className='px-3 py-2 '>History</button>
+        </li>
+        <li className='list-none'>
+          <button className='px-3 py-2 '>Watch Later</button>
+        </li>
+        <li className='list-none'>
+          <button className='px-3 py-2 '>Liked Videos</button>
+        </li>
+      </div>
+      <div className='p-2 border-b border-black'>
+        <h2 className='font-bold'>Subscriptions</h2>
+        <li className='list-none'>
+          <button className='px-3 py-2 '>One</button>
+        </li>
+        <li className='list-none'>
+          <button className='px-3 py-2 '>Two</button>
+        </li>
+        <li className='list-none'>
+          <button className='px-3 py-2 '>Three</button>
+        </li>
+        <li className='list-none'>
+          <button className='px-3 py-2 '>Four</button>
+        </li>
+        <li className='list-none'>
+          <button className='px-3 py-2 '>Five</button>
+        </li>
+      </div>
     </div>
   );
 };

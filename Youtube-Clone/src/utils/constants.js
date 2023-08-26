d09785323acf6939a3c46688b0678ffddcaa868e
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-26 22:14:53 +0530

    feat: add youtube api

---
+++ Youtube-Clone/src/utils/constants.js
@@ -0,0 +1,5 @@
+const YOUTUBE_API_KEY = "AIzaSyCOStjsHvGlOqO4NR-PfkbA89432dAzCkE";
+
+export const YOUTUBE_VIDEOS_URL =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&key=" +
+  YOUTUBE_API_KEY;

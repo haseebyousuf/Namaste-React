d48314d25c3ed242f78f84e0504af35707e46e1e
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-28 00:02:54 +0530

    refactor: fetch videos from api

--- Youtube-Clone/src/components/VideoContainer.jsx
+++ Youtube-Clone/src/components/VideoContainer.jsx
@@ -1,11 +1,23 @@
+import { useEffect } from "react";
 import FilterButtons from "./FilterButtons";
+import { YOUTUBE_VIDEOS_URL } from "../utils/constants";
+import { useState } from "react";
+import VideoCard from "./VideoCard";
 
 const VideoContainer = () => {
+  const [videos, setVideos] = useState();
+  useEffect(() => {
+    getVideos();
+  }, []);
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_VIDEOS_URL);
+    const json = await data.json();
+    setVideos(json.items);
+  };
   return (
-    <div>
-      {" "}
+    <div className='w-11/12'>
       <FilterButtons />
-      <h1>Video Container</h1>
+      {videos && <VideoCard data={videos[0]} />}
     </div>
   );
 };

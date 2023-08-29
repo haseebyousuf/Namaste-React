a6a872150b37371b1a95edb5cb5d45d85ebe36f1
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-29 21:08:01 +0530

    refactor: add dynamic filter list

--- Youtube-Clone/src/components/FilterButtons.jsx
+++ Youtube-Clone/src/components/FilterButtons.jsx
@@ -1,10 +1,25 @@
+import Button from "./Button";
+
+const filterList = [
+  "All",
+  "Music",
+  "Superhero Movies",
+  "Javascript",
+  "Computer Program",
+  "News",
+  "Tanmay Bhat",
+  "Apple",
+];
 const FilterButtons = () => {
   return (
-    <div className='flex justify-start gap-2'>
-      <button className='px-3 py-1 rounded-lg bg-slate-300'>Filter1</button>
-      <button className='px-3 py-1 rounded-lg bg-slate-300'>Filter2</button>
-      <button className='px-3 py-1 rounded-lg bg-slate-300'>Filter3</button>
-      <button className='px-3 py-1 rounded-lg bg-slate-300'>Filter4</button>
+    <div className='flex w-4/5 gap-2'>
+      {filterList.map((button, index) => {
+        return (
+          <div className='inline' key={index}>
+            <Button name={button} />
+          </div>
+        );
+      })}
     </div>
   );
 };

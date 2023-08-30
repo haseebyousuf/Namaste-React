d3c34ac128edcafd030819182c6d030baff150e0
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-30 18:19:35 +0530

    fix: removed api key

--- Youtube-Clone/src/utils/constants.js
+++ Youtube-Clone/src/utils/constants.js
@@ -1,5 +1,3 @@
-const YOUTUBE_API_KEY = "AIzaSyCOStjsHvGlOqO4NR-PfkbA89432dAzCkE";
-
 export const YOUTUBE_VIDEOS_URL =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&key=" +
-  YOUTUBE_API_KEY;
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=" +
+  import.meta.env.VITE_YOUTUBE_API_KEY;

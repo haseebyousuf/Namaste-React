832b98f56f4ff57b51b595a270dd8365790db6c3
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-01 11:56:48 +0530

    feat: add searchSuggestion API

--- Youtube-Clone/src/utils/constants.js
+++ Youtube-Clone/src/utils/constants.js
@@ -1,3 +1,6 @@
 export const YOUTUBE_VIDEOS_URL =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=" +
   import.meta.env.VITE_YOUTUBE_API_KEY;
+
+export const YOUTUBE_SEARCH_API =
+  "https://corsproxy.io/?http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";

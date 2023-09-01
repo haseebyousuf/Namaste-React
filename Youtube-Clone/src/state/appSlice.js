cecf0795ea1637ce717767d7c5404c3cf45861cc
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-01 11:55:08 +0530

    feat: add closeSidebar action to appSlice

--- Youtube-Clone/src/state/appSlice.js
+++ Youtube-Clone/src/state/appSlice.js
@@ -8,8 +8,11 @@ const appSlice = createSlice({
     toggleSidebar: (state) => {
       state.isSidebarOpen = !state.isSidebarOpen;
     },
+    closeSidebar: (state) => {
+      state.isSidebarOpen = false;
+    },
   },
 });
 
-export const { toggleSidebar } = appSlice.actions;
+export const { toggleSidebar, closeSidebar } = appSlice.actions;
 export default appSlice.reducer;

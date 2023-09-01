90d131bb502ca6b42dcaad003c2bfb02955cdcf1
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-01 11:56:00 +0530

    refactor: add searchSlice to store

--- Youtube-Clone/src/state/store.js
+++ Youtube-Clone/src/state/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appSlice from "./appSlice";
+import searchSlice from "./searchSlice";
 const store = configureStore({
   reducer: {
     app: appSlice,
+    search: searchSlice,
   },
 });
 

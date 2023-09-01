d9dabad239322cdc4a062ab6faef916cf56a4e50
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-01 11:54:05 +0530

    refactor: change styles and add onClick on home

--- Youtube-Clone/src/components/Sidebar.jsx
+++ Youtube-Clone/src/components/Sidebar.jsx
@@ -1,47 +1,87 @@
+import { useNavigate } from "react-router-dom";
+
 const Sidebar = () => {
+  const navigate = useNavigate();
   return (
-    <div className='w-40 mr-3 overflow-clip'>
-      <div className='p-2 border-b border-black'>
-        <li className='list-none'>
-          <button className='px-3 py-2 '>Home</button>
+    <div className='w-2/12 mr-3 overflow-clip'>
+      <div className='p-2 border-b border-gray-400'>
+        <li className='w-full list-none'>
+          <button
+            onClick={() => navigate("/")}
+            className='w-full px-3 py-2 rounded-lg text-start hover:bg-slate-200'
+          >
+            Home
+          </button>
         </li>
-        <li className='list-none'>
-          <button className='px-3 py-2 '>Shorts</button>
+        <li className='w-full list-none'>
+          <button className='w-full px-3 py-2 rounded-lg text-start hover:bg-slate-200 '>
+            Shorts
+          </button>
         </li>
-        <li className='list-none'>
-          <button className='px-3 py-2 '>Subscriptions</button>
-        </li>
-      </div>
-      <div className='p-2 border-b border-black'>
-        <li className='list-none'>
-          <button className='px-3 py-2 '>Library</button>
-        </li>
-        <li className='list-none'>
-          <button className='px-3 py-2 '>History</button>
-        </li>
-        <li className='list-none'>
-          <button className='px-3 py-2 '>Watch Later</button>
-        </li>
-        <li className='list-none'>
-          <button className='px-3 py-2 '>Liked Videos</button>
+        <li className='w-full list-none'>
+          <button className='w-full px-3 py-2 rounded-lg text-start hover:bg-slate-200 '>
+            Subscriptions
+          </button>
         </li>
       </div>
-      <div className='p-2 border-b border-black'>
+      <div className='p-2 border-b border-gray-400'>
+        <li className='w-full list-none'>
+          <button className='w-full px-3 py-2 rounded-lg text-start hover:bg-slate-200 '>
+            Library
+          </button>
+        </li>
+        <li className='w-full list-none'>
+          <button className='w-full px-3 py-2 rounded-lg text-start hover:bg-slate-200 '>
+            History
+          </button>
+        </li>
+        <li className='w-full list-none'>
+          <button className='w-full px-3 py-2 rounded-lg text-start hover:bg-slate-200 '>
+            Watch Later
+          </button>
+        </li>
+        <li className='w-full list-none'>
+          <button className='w-full px-3 py-2 rounded-lg text-start hover:bg-slate-200 '>
+            Liked Videos
+          </button>
+        </li>
+      </div>
+      <div className='p-2 border-b border-gray-400'>
         <h2 className='font-bold'>Subscriptions</h2>
-        <li className='list-none'>
-          <button className='px-3 py-2 '>One</button>
+        <li className='w-full list-none'>
+          <button className='w-full px-3 py-2 rounded-lg text-start hover:bg-slate-200 '>
+            One
+          </button>
         </li>
-        <li className='list-none'>
-          <button className='px-3 py-2 '>Two</button>
+        <li className='w-full list-none'>
+          <button className='w-full px-3 py-2 rounded-lg text-start hover:bg-slate-200 '>
+            Two
+          </button>
         </li>
-        <li className='list-none'>
-          <button className='px-3 py-2 '>Three</button>
+        <li className='w-full list-none'>
+          <button className='w-full px-3 py-2 rounded-lg text-start hover:bg-slate-200 '>
+            Three
+          </button>
         </li>
-        <li className='list-none'>
-          <button className='px-3 py-2 '>Four</button>
+        <li className='w-full list-none'>
+          <button className='w-full px-3 py-2 rounded-lg text-start hover:bg-slate-200 '>
+            Four
+          </button>
         </li>
-        <li className='list-none'>
-          <button className='px-3 py-2 '>Five</button>
+        <li className='w-full list-none'>
+          <button className='w-full px-3 py-2 rounded-lg text-start hover:bg-slate-200 '>
+            Five
+          </button>
+        </li>
+        <li className='w-full list-none'>
+          <button className='w-full px-3 py-2 rounded-lg text-start hover:bg-slate-200 '>
+            Five
+          </button>
+        </li>
+        <li className='w-full list-none'>
+          <button className='w-full px-3 py-2 rounded-lg text-start hover:bg-slate-200 '>
+            Five
+          </button>
         </li>
       </div>
     </div>

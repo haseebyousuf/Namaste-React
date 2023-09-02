d64d4b900f198e907ef65f15f86d2601a595531d
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-02 11:54:29 +0530

    refactor: include chatSlice

--- Youtube-Clone/src/state/store.js
+++ Youtube-Clone/src/state/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appSlice from "./appSlice";
 import searchSlice from "./searchSlice";
+import chatSlice from "./chatSlice";
 const store = configureStore({
   reducer: {
     app: appSlice,
     search: searchSlice,
+    chat: chatSlice,
   },
 });
 

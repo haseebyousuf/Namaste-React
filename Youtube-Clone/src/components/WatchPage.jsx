68589ca4b093487c1936c1803225a255731e0954
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-02 11:53:33 +0530

    refactor: add live chat to watch page

--- Youtube-Clone/src/components/WatchPage.jsx
+++ Youtube-Clone/src/components/WatchPage.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { closeSidebar, toggleSidebar } from "../state/appSlice";
 import { useSearchParams } from "react-router-dom";
 import CommentsContainer from "./CommentsContainer";
+import LiveChat from "./LiveChat";
 
 const WatchPage = () => {
   const dispatch = useDispatch();
@@ -17,15 +18,19 @@ const WatchPage = () => {
   }, []);
   return (
     <div className='px-5 py-1'>
-      <iframe
-        width='950'
-        height='500'
-        src={"https://www.youtube.com/embed/" + params.get("v")}
-        title='YouTube video player'
-        frameBorder='0'
-        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-        allowFullScreen
-      ></iframe>
+      <div className='flex gap-2'>
+        <iframe
+          width='950'
+          height='500'
+          src={"https://www.youtube.com/embed/" + params.get("v")}
+          title='YouTube video player'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+          allowFullScreen
+        ></iframe>
+        <div>
+          <LiveChat />
+        </div>
+      </div>
       <CommentsContainer />
     </div>
   );

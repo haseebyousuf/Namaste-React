b4c8de4088a6b9f744bed4bd5998ed9e48fd4694
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-03 20:46:02 +0530

    feat: add routes for demo components

--- Youtube-Clone/src/App.jsx
+++ Youtube-Clone/src/App.jsx
@@ -5,6 +5,8 @@ import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import WatchPage from "./components/WatchPage";
 import VideosContainer from "./components/VideosContainer";
+import Demo from "./components/Demo";
+import Demo2 from "./components/Demo2";
 function App() {
   return (
     <>
@@ -13,6 +15,15 @@ function App() {
           <Route path='/' element={<MainContainer />}>
             <Route path='/' element={<VideosContainer />} />
             <Route path='/watch' element={<WatchPage />} />
+            <Route
+              path='/demo'
+              element={
+                <>
+                  <Demo />
+                  <Demo2 />
+                </>
+              }
+            />
           </Route>
         </Routes>
       </BrowserRouter>

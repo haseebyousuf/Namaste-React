af2b36c6236bb52dc2bc5b03537700aa6db2ce03
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-03 20:46:26 +0530

    feat: add findNthPrime helper function

--- Youtube-Clone/src/utils/helper.js
+++ Youtube-Clone/src/utils/helper.js
@@ -178,3 +178,28 @@ var nameList = [
 export function generateRandomName() {
   return nameList[Math.floor(Math.random() * nameList.length)];
 }
+
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//Function to find the nth prime number
+export function findNthPrime(n) {
+  let count = 0;
+  let num = 2;
+  while (count < n) {
+    if (isPrime(num)) {
+      count++;
+    }
+    num++;
+  }
+  return num - 1;
+}

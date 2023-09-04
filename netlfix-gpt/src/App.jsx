db17f3680ec395325787ec4bb6aea8f604a3936e
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-04 14:17:55 +0530

    refactor: render body in App.js

--- netlfix-gpt/src/App.jsx
+++ netlfix-gpt/src/App.jsx
@@ -1,11 +1,8 @@
 import "./App.css";
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  );
+  return <Body />;
 }
 
 export default App;

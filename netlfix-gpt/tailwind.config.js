e85062cb9d974af38acfc9b45d6a968d18ed1795
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-04 15:05:07 +0530

    fix: background

--- netlfix-gpt/tailwind.config.js
+++ netlfix-gpt/tailwind.config.js
@@ -4,7 +4,8 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        "login-background": "url('./src/assets/background.jpg')",
+        "login-background":
+          "url('https://assets.nflxext.com/ffe/siteui/vlv3/855ed6e2-d9f1-4afd-90da-96023ec747c3/85eb5b91-25ed-4965-ace9-ba8e4a0ead8d/IN-en-20230828-popsignuptwoweeks-perspective_alpha_website_large.jpg')",
       },
     },
   },

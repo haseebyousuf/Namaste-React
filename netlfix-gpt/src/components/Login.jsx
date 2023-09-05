a3b6143fc96756a78bb7b4b69c09e9c0fa1e6dd6
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-05 15:32:48 +0530

    feat: implement firebase sign in and signup

--- netlfix-gpt/src/components/Login.jsx
+++ netlfix-gpt/src/components/Login.jsx
@@ -1,41 +1,136 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Header from "./Header";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { loginValidation } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import { login } from "../state/userSlice";
+
 const Login = () => {
   const [isSignInFrom, setIsSignInFrom] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const email = useRef(null);
+  const password = useRef(null);
+  const name = useRef();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handelFormSubmit = () => {
+    // setError({ ...error, message: null, isError: false });
+    const message = loginValidation(
+      email.current.value,
+      password.current.value,
+      isSignInFrom
+    );
+    setErrorMessage(message);
+    if (message) return;
+    if (isSignInFrom) {
+      setIsLoading(true);
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          navigate("/browse");
+        })
+        .catch((error) => {
+          setIsLoading(false);
+          setErrorMessage(error.message);
+        });
+    } else {
+      //sign up logic
+      setIsLoading(true);
+
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+          updateProfile(user, {
+            displayName: name.current.value,
+          })
+            .then(() => {
+              const { uid, email, displayName } = auth.currentUser;
+              dispatch(login({ uid, email, displayName }));
+              navigate("/browse");
+            })
+            .catch((error) => {
+              setIsLoading(false);
+              console.log(error);
+              setErrorMessage(error.message);
+            });
+        })
+        .catch((error) => {
+          setIsLoading(false);
+          setErrorMessage(error.message);
+        });
+    }
+  };
+
   return (
     <div className='bg-fill bg-login-background'>
       <Header />
       <div className=' flex items-center justify-center h-[100vh] bg-[rgba(0,0,0,.35)] '>
-        <form className='bg-[rgba(0,0,0,.80)] p-16 mx-3  flex flex-col rounded-sm'>
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          className='bg-[rgba(0,0,0,.80)] p-16 mx-3  flex flex-col rounded-sm'
+        >
           <h1 className='mb-6 text-4xl font-bold text-white'>
             {isSignInFrom ? "Sign In" : "Sign Up"}
           </h1>
+          {errorMessage && (
+            <div className='bg-[#e87c03] rounded-sm m-2 p-2 h-10 text-xs text-white flex items-center'>
+              {errorMessage}
+            </div>
+          )}
           {!isSignInFrom && (
             <>
               <input
+                ref={name}
                 type='text'
                 placeholder='Your Name'
                 className='px-5 py-4 m-2 rounded-sm bg-[#333] h-12 w-80 outline-none text-white'
               />
-              <span className='mx-2 text-sm text-yellow-600'></span>
+              <span className='text-sm text-yellow-600 '></span>
             </>
           )}
           <input
-            type='email'
+            ref={email}
+            type='text'
             placeholder='Email'
             className='px-5 py-4 m-2 rounded-sm bg-[#333] h-12 w-80 outline-none text-white'
           />
           <span className='mx-2 text-sm text-yellow-600'></span>
 
           <input
+            ref={password}
             type='password'
             placeholder='Password'
             className='px-5 py-4 m-2 rounded-sm bg-[#333] h-12 w-80 outline-none text-white'
           />
-          <button className='m-2 h-12 w-80 mt-6 bg-[#e50914] rounded-sm text-white font-bold cursor-pointer'>
-            Sign In
+          <button
+            type='submit'
+            disabled={isLoading}
+            onClick={handelFormSubmit}
+            className='m-2 h-12 w-80 mt-6 bg-[#e50914] rounded-sm text-white font-bold cursor-pointer disabled:bg-[#a8373d] '
+          >
+            {isLoading
+              ? "Please Wait..."
+              : isSignInFrom
+              ? "Sign In"
+              : "Sign Up"}
           </button>
           <div className='flex justify-between mx-2 my-1'>
             <span>
@@ -49,7 +144,10 @@ const Login = () => {
           <p className='m-2 text-[#b3b3b3]'>
             {isSignInFrom ? "New to Netflix?" : "Already Registered?"}
             <span
-              onClick={() => setIsSignInFrom(!isSignInFrom)}
+              onClick={() => {
+                setIsSignInFrom(!isSignInFrom);
+                setErrorMessage(null);
+              }}
               className='ml-2 text-white cursor-pointer hover:underline'
             >
               {isSignInFrom ? "Sign Up Now." : "Sign In Now."}

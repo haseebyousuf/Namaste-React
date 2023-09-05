d25d89627a54c78879fe2af2ed1e5138674f2c04
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-05 15:32:15 +0530

    feat: add redux store

---
+++ netlfix-gpt/src/state/appStore.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userSlice from "./userSlice";
+const appStore = configureStore({
+  reducer: {
+    user: userSlice,
+  },
+});
+
+export default appStore;

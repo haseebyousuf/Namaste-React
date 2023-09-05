98b46bba1744b9e467f263771752c475bda2aa8e
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-05 15:33:20 +0530

    feat: provide store to app

--- netlfix-gpt/src/App.jsx
+++ netlfix-gpt/src/App.jsx
@@ -1,8 +1,14 @@
+import { Provider } from "react-redux";
 import "./App.css";
 import Body from "./components/Body";
+import appStore from "./state/appStore";
 
 function App() {
-  return <Body />;
+  return (
+    <Provider store={appStore}>
+      <Body />
+    </Provider>
+  );
 }
 
 export default App;

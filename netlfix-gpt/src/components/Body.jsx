ae978da5a904e77b698540352de5f1c6a2ef6c53
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-05 15:34:26 +0530

    feat: add onAuthStateChanged listener and protect routes

--- netlfix-gpt/src/components/Body.jsx
+++ netlfix-gpt/src/components/Body.jsx
@@ -1,16 +1,40 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
 import Browse from "./Browse";
 import Login from "./Login";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout } from "../state/userSlice";
 
 const Body = () => {
+  const isAuth = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName } = user;
+        dispatch(login({ uid, email, displayName }));
+        // ...
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
+
   const appRouter = createBrowserRouter([
     {
       path: "/",
-      element: <Login />,
+      element: isAuth ? <Navigate to='/browse' /> : <Login />,
     },
     {
       path: "/browse",
-      element: <Browse />,
+      element: isAuth ? <Browse /> : <Navigate to='/' />,
     },
   ]);
   return (

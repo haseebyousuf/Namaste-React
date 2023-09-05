47a78b3e43fb754bb1a48cf15c78c01b72252c70
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-05 20:57:54 +0530

    refactor: add name validation for signup

--- netlfix-gpt/src/utils/validate.js
+++ netlfix-gpt/src/utils/validate.js
@@ -1,15 +1,20 @@
-export const loginValidation = (email, password, isSignInForm) => {
+export const loginValidation = (email, password, name, isSignInForm) => {
   const isValidEmail = /^\w+([\\.-]?\w+)*@\w+([\\.-]?\w+)*(\.\w{2,3})+$/.test(
     email
   );
-  if (!isSignInForm) {
-    const isPasswordValid =
-      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
-    if (!isPasswordValid) return "Choose a Strong Password";
-  }
 
   if (email.length <= 0) return "Email is required";
   else if (!isValidEmail) return "Email is invalid";
+
+  if (!isSignInForm) {
+    if (name.length <= 0) return "Name is required";
+
+    const isPasswordValid =
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+    if (password.length <= 0) {
+      return "Password is required";
+    } else if (!isPasswordValid) return "Choose a Strong Password";
+  }
 
   return null;
 };

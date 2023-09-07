fda045e7236da8f14a9dc0a7c489f34b39b415bd
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-07 22:51:47 +0530

    feat: add movie reducer to store

--- netlfix-gpt/src/state/appStore.js
+++ netlfix-gpt/src/state/appStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
+import moviesSlice from "./moviesSlice";
 const appStore = configureStore({
   reducer: {
     user: userSlice,
+    movies: moviesSlice,
   },
 });
 

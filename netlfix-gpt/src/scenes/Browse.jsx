d005589f9b959f35139f5c189abf1376cea70ce2
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-08 22:55:39 +0530

    refactor: use reusable custom hook

--- netlfix-gpt/src/scenes/Browse.jsx
+++ netlfix-gpt/src/scenes/Browse.jsx
@@ -1,21 +1,31 @@
 import { useSelector } from "react-redux";
 import PageBody from "../components/PageBody";
 import PageHeader from "../components/PageHeader";
-import { useGetTrendingMovies } from "../hooks/useGetTrendingMovies";
-import { useGetTrendingSeries } from "../hooks/useGetTrendingSeries";
-import { useGetTrendingTitles } from "../hooks/useGetTrendingTitles";
+import { useFetchMovies } from "../hooks/useFetchMovies";
+import {
+  addTrendingMovies,
+  addTrendingSeries,
+  addTrendingTitles,
+} from "../state/moviesSlice";
 
 const Browse = () => {
-  useGetTrendingTitles();
-  useGetTrendingMovies();
-  useGetTrendingSeries();
+  useFetchMovies(
+    "https://api.themoviedb.org/3/trending/movie/week?language=en-US",
+    addTrendingMovies
+  );
+  useFetchMovies(
+    "https://api.themoviedb.org/3/trending/all/week?language=en-US",
+    addTrendingTitles
+  );
+  useFetchMovies(
+    "https://api.themoviedb.org/3/trending/tv/week?language=en-US",
+    addTrendingSeries
+  );
   let allCategories = [];
   allCategories.push(useSelector((state) => state.movies.trendingTitles));
   allCategories.push(useSelector((state) => state.movies.trendingSeries));
   allCategories.push(useSelector((state) => state.movies.trendingMovies));
   const trendingTitles = useSelector((state) => state.movies.trendingTitles);
-  // const trendingMovies = useSelector((state) => state.movies.trendingMovies);
-  // const trendingSeries = useSelector((state) => state.movies.trendingSeries);
   return (
     trendingTitles.data && (
       <div className='absolute '>

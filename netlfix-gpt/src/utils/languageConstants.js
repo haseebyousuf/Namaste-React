8a838c177956a0752da97b3d2802c0ee7e62a4ba
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-10 10:26:16 +0530

    feat: add multi language support

---
+++ netlfix-gpt/src/utils/languageConstants.js
@@ -0,0 +1,32 @@
+export const lang = {
+  eng: {
+    signIn: "Sign In",
+    signUp: "Sign Up",
+    pleaseWait: "Please Wait",
+    search: "Search",
+    searchTitle: "Get Movies Suggestions from ChatGPT",
+    searchGptPlaceholder: "What would you like to watch today?",
+  },
+  hin: {
+    signIn: "साइन इन करें",
+    signUp: "शुरू करें",
+    pleaseWait: " प्रतीक्षा करें",
+    search: "खोज",
+    searchTitle: "Chat GPT से फिल्मों के सुझाव प्राप्त करें",
+    searchGptPlaceholder: "आज आप क्या देखना चाहेंगे?",
+  },
+  spa: {
+    signIn: "Iniciar Sesión",
+    signUp: "Inscribirse",
+    pleaseWait: "Esperar",
+    search: "Buscar",
+    searchTitle: "Obtenga recomendaciones de películas de Chat GPT",
+    searchGptPlaceholder: "Qué te gustaría ver hoy?",
+  },
+};
+
+export const SUPPORTED_LANGUAGES = [
+  { id: 1, identifier: "eng", name: "English" },
+  { id: 2, identifier: "hin", name: "Hindi" },
+  { id: 3, identifier: "spa", name: "Spanish" },
+];

ac48d011d8fe119e4b30624c092db345992b09a2
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-10 10:25:33 +0530

    feat: add config reducer to appStore

--- netlfix-gpt/src/state/appStore.js
+++ netlfix-gpt/src/state/appStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
 import moviesSlice from "./moviesSlice";
+import configSlice from "./configSlice";
 const appStore = configureStore({
   reducer: {
     user: userSlice,
     movies: moviesSlice,
+    config: configSlice,
   },
 });
 

019b24b7c9f755d604c20a11e6a57905d9ee429d
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-11 09:18:05 +0530

    refactor: send  media_type prop to the children

--- netlfix-gpt/src/components/PageHeader.jsx
+++ netlfix-gpt/src/components/PageHeader.jsx
@@ -3,11 +3,23 @@ import VideoTitle from "./VideoTitle";
 
 const PageHeader = (movie) => {
   if (!movie) return null;
-  const { title, name, overview, id, poster_path } = movie.movie;
+  const { title, name, overview, id, poster_path, first_air_date } =
+    movie.movie;
+  const type = first_air_date ? "tv" : "movie";
   return (
     <div className='pt-[23%] sm:pt-[30%] bg-black md:pt-0  '>
-      <VideoTitle title={title || name} overview={overview} />
-      <VideoBackground videoId={id} poster_path={poster_path} title={title} />
+      <VideoTitle
+        title={title || name}
+        overview={overview}
+        id={id}
+        first_air_date={first_air_date}
+      />
+      <VideoBackground
+        videoId={id}
+        poster_path={poster_path}
+        title={title}
+        media_type={type}
+      />
     </div>
   );
 };

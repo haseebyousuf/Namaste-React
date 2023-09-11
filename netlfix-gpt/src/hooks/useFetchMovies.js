85273be577940b3a75a47a07110b21d077185705
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-11 09:21:34 +0530

    refactor: prevent api calls if data is already present

--- netlfix-gpt/src/hooks/useFetchMovies.js
+++ netlfix-gpt/src/hooks/useFetchMovies.js
@@ -1,16 +1,20 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { GET_OPTIONS } from "../utils/constants";
 import { useEffect } from "react";
 
-export const useFetchMovies = (url, action) => {
+export const useFetchMovies = (url, action, stateValue) => {
+  const movies = useSelector((state) => state.movies[stateValue]);
+
   const dispatch = useDispatch();
+  useEffect(() => {
+    getData();
+  }, []);
   const getData = async () => {
+    if (movies.data) {
+      return;
+    }
     const response = await fetch(url, GET_OPTIONS);
     const data = await response.json();
     dispatch(action(data.results));
   };
-
-  useEffect(() => {
-    getData();
-  }, []);
 };

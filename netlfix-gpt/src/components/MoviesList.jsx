3551ce2c3ca5c732d67226f175ff5197950bb9ce
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-09-11 09:17:24 +0530

    refactor: hide vertical scroll

--- netlfix-gpt/src/components/MoviesList.jsx
+++ netlfix-gpt/src/components/MoviesList.jsx
@@ -8,8 +8,8 @@ const MoviesList = ({ title, movies }) => {
       <h1 className='pt-1.5 text-base font-bold leading-6 text-white sm:pt-2 sm:text-lg'>
         {title}
       </h1>
-      <div className='flex overflow-x-auto movies-list'>
-        <div className='flex gap-2'>
+      <div className='flex overflow-x-auto overflow-y-hidden movies-list scroll-smooth'>
+        <div className='flex gap-4'>
           {movies?.map((movie) => (
             <MovieCard key={movie.id} movie={movie} />
           ))}
